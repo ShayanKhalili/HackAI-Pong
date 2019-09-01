7213c91f7d6e1eaf7b056c266ab040e29360afc5
Shayanlion <Shayanlion25@gmail.com>
2019-09-01 12:04:09 -0400

    Updated networking logistics of the game for communication between the game, server, and client. Implemented the time limit for sending a move.

--- server.js
+++ server.js
@@ -6,47 +6,66 @@ const io = require('socket.io')(http);
 let player1 = null;
 let player2 = null;
 let gameSocket = null;
-let gameUpdateFunction = null;
 let moves = {
     p1: null,
     p2: null
 }
+let movesTimeout = null;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
 
-let game = io.of('/game');
-game.on('connection', function(socket) {
+function cancelMoveRequest() {
+    if (moves.p1 === null) player1.emit('cancel move');
+    if (moves.p2 === null) player2.emit('cancel move');
+    if (gameSocket != null) gameSocket.emit('update', moves);
+    moves.p1 = null;
+    moves.p2 = null;
+}
+
+let gameNamespace = io.of('/game');
+gameNamespace.on('connection', function(socket) {
         console.log('game connection established');
         gameSocket = socket;
-        socket.on('request moves', function(gameData, fn){
+        socket.on('request moves', function(gameData){
             if(player1 != null && player2 != null) {
                 player1.emit('request move', gameData);
                 player2.emit('request move', gameData);
-                gameUpdateFunction = fn;
+                movesTimeout = setTimeout(cancelMoveRequest, 200);
             }
         });
 
-        if (player1 != null && player2 != null) {
-            gameSocket.emit('force game loop');
-        }
+        socket.on('disconnect', function(){
+            gameSocket = null;
+        });
+
+        // if (player1 != null && player2 != null) {
+        //     gameSocket.emit('force game loop');
+        // }
     });
 
-let players = io.of('/player');
-players.on('connection', function(socket){
+let playerNamespace = io.of('/player');
+playerNamespace.on('connection', function(socket){
     console.log('a user connected');
     if (player1 === null) {
         player1 = socket;
+        if (player2 != null && gameSocket != null) {
+            gameSocket.emit('force game loop');
+        }
     } else if (player2 === null) {
         player2 = socket;
+        if (player1 != null && gameSocket != null) {
+            gameSocket.emit('force game loop');
+        }
     }
 
     socket.on('move', function(data){
         moves[data.player] = data.direction;
         console.log(moves);
         if (moves.p1 != null && moves.p2 != null) {
-            gameUpdateFunction(moves.p1, moves.p2);
+            if (gameSocket != null) gameSocket.emit('update', moves);
+            clearTimeout(movesTimeout);
             moves.p1 = null;
             moves.p2 = null;
         }
